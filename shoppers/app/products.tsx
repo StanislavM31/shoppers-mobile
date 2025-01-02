2b6d3b3c5317dfa81a22b9a5f7ada3ed105c0483
Stanislav Minko <minkostas@gmail.com>
2025-01-02 20:31:59 +0300

    add back button

--- shoppers/app/products.tsx
+++ shoppers/app/products.tsx
@@ -1,7 +1,7 @@
 import storage from '../storage/index'
 import { iProduct } from '@/interfaces';
 
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 
 
@@ -9,23 +9,24 @@ function Products() {
     const router = useRouter()
 
     const container = storage.map((el: iProduct) =>
-        <TouchableOpacity key={el.id} >
+        <View key={el.id} >
             <View style={styles.item} >
                 {el?.img}
                 <Text style={{ textAlign: 'center', marginTop: 10 }}>{el?.title}</Text>
                 <Text style={{ textAlign: 'center' }}>{el?.price}</Text>
             </View>
-        </TouchableOpacity>
+        </View>
     )
 
-    return <View style={{ gap: 62, flex: 1 }}>
-
-
+    return <>
+    <View style={{ gap: 62, flex: 1 }}>
         <View style={{ flexDirection: 'row', gap: 60, justifyContent: 'center', flexWrap: 'wrap' }}>
             {container}
         </View>
 
     </View>
+    <Button title='Back' onPress={()=>router.back()}/>
+    </>
 }
 
 const styles = StyleSheet.create({
