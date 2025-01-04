a4a2359da5e9f7d7bf84c216370ac5014c891b46
Stanislav Minko <minkostas@gmail.com>
2025-01-04 23:58:47 +0300

    tabs

--- shoppers/app/_layout.tsx
+++ shoppers/app/_layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout() {
 {/*         <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
         <Stack.Screen name="/login" />
+        <Stack.Screen name="(tabs)"/>
         <Stack.Screen name="/register" />
         <Stack.Screen name="/products" />
       </Stack>
